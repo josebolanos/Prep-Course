2abdc3dd7ef1f8129e9c47a6c18fc4db491bf64a
Jose <josebolanoslanzarote@gmail.com>
2022-09-12 19:07:11 +0100

    first time array

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,24 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  array[1,2,3,4,5];
+    return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  array[1,2,3,4,5];
+  return array[array.length -1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  array[1,2,3,4,5];
+  return array.length;
 }
 
 
@@ -23,6 +29,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  nuevoArray = [];
+   for( i = 0; i < array.length; i++) {
+      nuevoArray[i] = array[i] + 1;
+   }
+   return nuevoArray;
 }
 
 
